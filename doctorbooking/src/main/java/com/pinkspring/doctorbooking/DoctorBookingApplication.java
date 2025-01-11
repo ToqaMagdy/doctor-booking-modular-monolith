88482e8038304ea5124245d9c7a9b8d76ed5f0e4
Marwa Elsayed <marwa.elsayed@ibm.com>
2025-01-11 21:46:49 +0200

    follow case camel in file name

--- doctorbooking/src/main/java/com/pinkspring/doctorbooking/DoctorBookingApplication.java
+++ doctorbooking/src/main/java/com/pinkspring/doctorbooking/DoctorBookingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoctorbookingApplication {
+public class DoctorBookingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoctorbookingApplication.class, args);
+		SpringApplication.run(DoctorBookingApplication.class, args);
 	}
 
 }
