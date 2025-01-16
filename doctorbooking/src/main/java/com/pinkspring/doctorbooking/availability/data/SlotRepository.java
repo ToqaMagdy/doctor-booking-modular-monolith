9aee77e400092f62a2defe9f62d2f0c93741a165
Marwa Elsayed <marwa.elsayed@ibm.com>
2025-01-16 21:47:18 +0200

    Adding getAllUpComingSlots

--- doctorbooking/src/main/java/com/pinkspring/doctorbooking/availability/data/SlotRepository.java
+++ doctorbooking/src/main/java/com/pinkspring/doctorbooking/availability/data/SlotRepository.java
@@ -2,8 +2,15 @@ package com.pinkspring.doctorbooking.availability.data;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SlotRepository extends JpaRepository<Slot, UUID> {
+    @Query(value ="select s.id,s.doctorName, s.isReserved, s.time, s.cost from Slot s where s.isReserved = false", nativeQuery = true)
+    List<Slot> findDoctorAvailableSlots();
+
+    @Query(value ="select s.id,s.doctorName, s.isReserved, s.time, s.cost from Slot s where s.time > CURRENT_TIMESTAMP", nativeQuery = true)
+    List<Slot> findUpComingSlots();
 }
